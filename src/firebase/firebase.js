e0e0cc7c6abc6a2b88b457faf024655ad4db727c
Chukwudi Stone <mpara3100@gmail.com>
2026-05-20 04:16:10 +0100

    Initial client setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,23 @@
-// src/firebase/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+// Client Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDpS_93zmm4cWS5rD42DN_8AL7CQhvCWto",
+  authDomain: "web3-51388.firebaseapp.com",
+  projectId: "web3-51388",
+  storageBucket: "web3-51388.firebasestorage.app",
+  messagingSenderId: "108293241627",
+  appId: "1:108293241627:web:c6ee0f65f36a921ef6b1be",
+  measurementId: "G-D0MS9HKPQY",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// Initialize services
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
 export default app;
